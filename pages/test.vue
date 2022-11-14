0144a98829623fe7cdb0101f2e1f8197ae7732f2
danwritecode <dnelson4993@outlook.com>
2022-11-14 08:54:15 -0500

    image download backup

---
+++ pages/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img 
+      id="diff-image"
+      src="https://replicate.delivery/pbxt/ygtueiTM5A1HIK9YGmNsp81h2l1gRb9FEBfJXsJgzEQ9ld8PA/out-0.png"  
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+if(process.client) {
+  const img = document.getElementById("diff-image")
+  const arrayBuffer = await img.arrayBuffer()
+  console.log(arrayBuffer)
+}
+
+</script>
